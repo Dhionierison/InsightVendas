94c1a8a96d1f74691d22702035537b0eef09980a
dhionierison <dhionierison@gmail.com>
2025-07-25 13:57:51 -0400

    adiantada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+import { Sidebar } from "../components/sidebar";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,9 +28,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={cn("min-h-screen bg-background font-sans antialiased", geistSans.variable)}>
+        <Sidebar />
         {children}
       </body>
     </html>
